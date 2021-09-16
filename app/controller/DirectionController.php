f9afe4ed8791dbf02513f6460881d64eca7bcd4d
Franko Vekić <franko.vekic@gmail.com>
2021-09-16 13:45:23 +0200

    Made minor changes.

--- app/controller/DirectionController.php
+++ app/controller/DirectionController.php
@@ -8,14 +8,21 @@ class DirectionController extends AuthorizeController
     private $game;
     private $message;
 
-    
-    public function games() 
+    public function add()
     {
-        $this->view->render($this->viewDir . 'games',['games'=>Games::read()]);
-    } 
-
+        $this->view->render($this->viewDir . 'new',[
+            "game" =>$this->game,
+            'message'=>'Enter required data.'
+        ]);
+    }
+    
     public function admin() 
     {
-        $this->view->render($this->viewDir . 'admin');
+        $this->view->render($this->viewDir . 'admin',['games'=>Games::read()]);
+    } 
+
+    public function games() 
+    {
+        $this->view->render($this->viewDir . 'games');
     } 
 }
